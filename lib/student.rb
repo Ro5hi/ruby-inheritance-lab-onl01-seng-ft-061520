25dd7b1b43f6546bc0a3c4ebcffe08d119af23f9
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-01 21:47:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,10 @@
 class Student < User 
   
-  attr_accessor :knowledge
-  
-  def initialize 
-    @knowledge = []
-  end 
-  
+ attr_accessor :knowledge
 
+  def initialize
+    @knowledge = []
+  end
+  
+  
 end
